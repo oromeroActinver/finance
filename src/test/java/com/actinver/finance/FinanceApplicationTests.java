1c92ac8340d5cf7af59d545b0b21e2ab7d9308eb
oromeroActinver <oromero@actinver.com.mx>
2024-04-29 16:28:13 -0600

    first commit

---
+++ src/test/java/com/actinver/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actinver.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

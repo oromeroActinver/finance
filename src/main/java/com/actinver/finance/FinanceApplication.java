a43298f12463fa16cd5d0fe9069a9214495c2f68
oromeroActinver <oromero@actinver.com.mx>
2024-04-30 11:25:09 -0600

    config create war deploy wlyfly

--- src/main/java/com/actinver/finance/FinanceApplication.java
+++ src/main/java/com/actinver/finance/FinanceApplication.java
@@ -2,12 +2,19 @@ package com.actinver.finance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FinanceApplication {
+public class FinanceApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FinanceApplication.class);
+    }
 
 }
